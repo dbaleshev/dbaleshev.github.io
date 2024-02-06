b951c0215677a8490de7c4fcb5791727f10648ee
dbaleshev <dbaleshev@torontomu.ca>
2024-02-05 21:04:34 -0500

    Lab 4 - JS

---
+++ main.js
@@ -0,0 +1,15 @@
+var greeting = "Hello ";
+
+var name = "Class";
+
+alert(greeting + name);
+
+username = window.prompt("What is Your Name?");
+
+function changebackground (color) {
+    document.body.style.background = color
+}
+
+window.addEventListener("load", function() {changebackground('blue')});
+
+console.log(username);

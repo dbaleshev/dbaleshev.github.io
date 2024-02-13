41754e18ba79928606ba4c7270284a59f68033e2
dbaleshev <dbaleshev@torontomu.ca>
2024-02-12 20:49:45 -0500

    inclass lab

--- main.js
+++ main.js
@@ -13,3 +13,22 @@ function changebackground (color) {
 window.addEventListener("load", function() {changebackground('blue')});
 
 console.log(username);
+
+
+for (let i = 0; i < 5; i++) {
+    alert("Hello")
+}
+
+function myFunction(){
+    alert("Hello!");
+    }
+
+function mOver(some_tag) {
+        some_tag.innerHTML = "Thank You”;
+        some_tag.style.color = "blue";
+    }
+        
+function mOut(element) {
+     some_tag.innerHTML = "Mouse Over Me";
+      some_tag.style.color = "orange";
+    }
